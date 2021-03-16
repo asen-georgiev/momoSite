41c9b7d8d2256c38098c57802aa89e3e1e9d16c7
Asen Georgiev <asen.georgiev@devhost.bg>
2021-03-16 13:49:13 +0200

    Styling the BlogPage.

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -2,11 +2,21 @@ import React, {Component} from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {Link} from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CardImg from "react-bootstrap/CardImg";
 import {getBlogs} from "../../services/blogService";
+import "../../css/blog/blogPage.css";
+import Image from "react-bootstrap/Image";
+import maria from "../../assets/maria.jpg";
+import {faPhoneSquare} from "@fortawesome/free-solid-svg-icons/faPhoneSquare";
+import {faMailBulk} from "@fortawesome/free-solid-svg-icons/faMailBulk";
+import {faFacebookSquare} from "@fortawesome/free-brands-svg-icons/faFacebookSquare";
+import {faInstagramSquare} from "@fortawesome/free-brands-svg-icons/faInstagramSquare";
+import {faTwitterSquare} from "@fortawesome/free-brands-svg-icons/faTwitterSquare";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons/faLinkedin";
 
 class Blog extends Component {
     constructor(props) {
@@ -27,8 +37,92 @@ class Blog extends Component {
     render() {
         return (
             <div>
-                <Container className="bg-danger" style={{height:'auto'}} fluid={true}>
+                <Container className="blogpage-main" fluid={true}>
+                    <Row className="blogpage-title-row justify-content-center align-content-center">
+                        <Col className="d-flex flex-column text-center">
+                            <span className="blogpage-title-span">
+                                MOMO DESIGN BLOG
+                            </span>
+                            <span className="blogpage-subtitle-span">
+                                Donec in libero nec magna molestie dignissim. Aenean placerat porta laoreet.
+                            </span>
+                        </Col>
+                    </Row>
                     <Row>
+                        <Col className="blogpage-info-col">
+                            <Row>
+                                <Col md="auto" className='p-0'>
+                                    <Image
+                                        className="p-4"
+                                        src={maria}
+                                        style={{width: 430, height: "auto"}}/>
+                                </Col>
+                                <Col className='d-flex flex-column p-4'>
+                                    <ul className="blogpage-ul">
+                                        <li>name : MARIA TASHKOVA</li>
+                                        <li>born : 19.09.1979</li>
+                                        <li>nationality : BULGARIAN</li>
+                                        <li>education : ECONOMICS AND MANAGEMENT</li>
+                                        <li>current : GRAPHIC DESIGNER / PHOTOGRAPHER</li>
+                                    </ul>
+                                    <ul className="blogpage-ul">
+                                        <li>
+                                            <Link>
+                                                <FontAwesomeIcon
+                                                    className="icon mr-2"
+                                                    icon={faFacebookSquare}/>
+                                                <span>Facebook</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link>
+                                                <FontAwesomeIcon
+                                                    className="icon mr-2"
+                                                    icon={faInstagramSquare}/>
+                                                <span>Instagram</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link>
+                                                <FontAwesomeIcon
+                                                    className="icon mr-2"
+                                                    icon={faTwitterSquare}/>
+                                                <span>Twitter</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link>
+                                                <FontAwesomeIcon
+                                                    className="icon mr-2"
+                                                    icon={faLinkedin}/>
+                                                <span>LinkedIn</span>
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                    <ul className="blogpage-ul">
+                                        <li>
+                                            <Link>
+                                                <FontAwesomeIcon
+                                                    className="icon mr-2"
+                                                    icon={faPhoneSquare}/>
+                                                <span>+359/123456789</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link>
+                                                <FontAwesomeIcon
+                                                    className="icon mr-2"
+                                                    icon={faMailBulk}/>
+                                                <span>tashkovamaria@gmail.com</span>
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </Col>
+                            </Row>
+                            <Row className="bg-info" style={{height:100}}>
+                                Take a look at my work!
+                            </Row>
+                        </Col>
                         <Col>
                             {this.state.blogs.map(blog => {
                                     return (
@@ -46,7 +140,7 @@ class Blog extends Component {
                                             </Card.Body>
                                             <Card.Footer>
                                                 <Link to={`/blog/${blog._id}`}>
-                                                    Read the whole article
+                                                    Continue reading...
                                                 </Link>
                                             </Card.Footer>
                                         </Card>
